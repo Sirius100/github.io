1d26cf034d65d8bc6ce25e06bf569a1efcbe0a77
ESA <esaul-00@mail.ru>
2019-09-17 00:21:53 +0300

    добавил border в меню

--- js/eff_menu.js
+++ js/eff_menu.js
@@ -1,21 +1,21 @@
-/*window.addEventListener('load', function()  {
+window.addEventListener('load', function()  {
 	const item = document.querySelector('#menu');
 	item.checked = false;
-}, false);*/
- 
 
-function check (){
-	const submenu = document.querySelector('.submenu');
-	const item = document.querySelector('#menu');
+	function check (){
+		const submenu = document.querySelector('.submenu');
+		const item = document.querySelector('#menu');
 
-	if (item.checked == false){ 	
-		submenu.style.display = "flex";
-		submenu.style.transform = 'translateY(-5rem)'	
-		submenu.style.opacity = "1";
-	}
-	else {
-		submenu.style.transform = 'translateY(0rem)';
-		submenu.style.opacity = "0";
-	}
-};
-
+			if (item.checked != false){ 	
+				submenu.style.display = "flex";
+				submenu.style.transition = 'all 1.5s linear .2s';
+				submenu.style.transform = 'translateY(-5rem)';	
+				submenu.style.opacity = "1";
+			}
+			else {
+				submenu.style.transform = 'translateY(0rem)';
+				submenu.style.opacity = "0";
+			}
+	};
+return check();
+}, false);
