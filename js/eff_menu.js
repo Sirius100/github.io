ccdbff56bc8fb90f46f7c02dbd667ed81b82d4fd
ESA <esaul-00@mail.ru>
2019-09-18 20:43:15 +0300

    переписал  для корректной работы кнопки меню eff_menu.js

--- js/eff_menu.js
+++ js/eff_menu.js
@@ -1,22 +1,32 @@
 /*window.addEventListener('load', function()  {
 	const item = document.querySelector('#menu');
-	item.checked = false;
+	console.log(item.checked);
+	item.checked = true;
 }, false);*/
  
+const item = document.querySelector('#menu');
+/**/item.checked = true;
+console.log('item.checked загрузка сайта и = ', item.checked);
+
+
 
 function check (){
 	const submenu = document.querySelector('.submenu');
 	const item = document.querySelector('#menu');
 
-		if (item.checked == false){ 	
+		if (item.checked != true){ 	
+			console.log('item.checked вошел в секцию true и = ', item.checked);
 			submenu.style.display = "flex";
 			submenu.style.transition = 'all 1.5s linear .2s';
 			submenu.style.transform = 'translateY(-5rem)';	
 			submenu.style.opacity = "1";
+			console.log('item.checked вышел из секции true и = ', item.checked);
 		}
 		else {
+			console.log('item.checked вошел в секцию else и = ', item.checked);
 			submenu.style.transform = 'translateY(0rem)';
 			submenu.style.opacity = "0";
+			console.log('item.checked вышел из секции else и = ', item.checked);
 		}
 };
 
